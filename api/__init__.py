a35063e0a255ff17425e5273c34d86003f16665f
DavidCuy <david.cuy.sanchez@gmail.com>
2023-09-27 00:03:27 -0600

    fix: mongo router missing reference

--- api/__init__.py
+++ api/__init__.py
@@ -34,12 +34,11 @@ def create_app():
     from .routes.MqttTopicRouter import mqtt_service
     from .routes.DeviceTypeRouter import deviceType_service
     from .routes.DeviceRouter import device_service
-    from .routes import mqtt_router, device_router, deviceType_router, mongo_test_router
+    from .routes import mqtt_router, device_router, deviceType_router
 
     app.register_blueprint(mqtt_router, url_prefix=f'/{env.STAGE}/{mqtt_service.get_model_path_name()}')
     app.register_blueprint(deviceType_router, url_prefix=f'/{env.STAGE}/{deviceType_service.get_model_path_name()}')
     app.register_blueprint(device_router, url_prefix=f'/{env.STAGE}/{device_service.get_model_path_name()}')
-    app.register_blueprint(mongo_test_router, url_prefix=f'/{env.STAGE}/mongo-test')
     
 
     @app.errorhandler(APIException)
--- api/routes/__init__.py
+++ api/routes/__init__.py
@@ -1,4 +1,3 @@
 from .DeviceTypeRouter import deviceType_router
 from .DeviceRouter import device_router
 from .MqttTopicRouter import mqtt_router
-from .MongoTestRouter import mongo_test_router
